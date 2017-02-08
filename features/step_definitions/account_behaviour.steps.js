2b1f67c809a87ecb99ed9d1d307022e12039d6e2
Gabriel Viso Carrera <gvisoc@gmail.com>
2017-02-08 09:23:50 +0100

    Adding eslint

--- features/step_definitions/account_behaviour.steps.js
+++ features/step_definitions/account_behaviour.steps.js
@@ -1,26 +1,29 @@
+/* eslint max-statements: "off", new-cap: "off" */
 'use strict';
-const {defineSupportCode} = require ('cucumber');
-const assert = require ('assert');
+const {defineSupportCode} = require('cucumber');
+const assert = require('assert');
+var Account = require('../../entities/account');
 
-var Account = require ('../../entities/account');
+defineSupportCode(({Given, Then, When}) => {
+  var myAccount = null;
 
-defineSupportCode (function ({Given, Then, When}) {
-  var myAccount;
+  Given('an account with balance {arg1:float}', (arg1, callback) => {
+    myAccount = Account.create(arg1);
+    callback();
+  });
 
-  Given ('an account with balance {arg1:float}', function (arg1, callback) {
-    myAccount = Account.create (arg1);
-    callback ();
+  When('I make a deposit of {arg1:float}', (arg1, callback) => {
+    myAccount.deposit(arg1);
+    callback();
   });
-  When ('I make a deposit of {arg1:float}', function (arg1, callback) {
-    myAccount.deposit (arg1);
-    callback ();
+
+  When('I make a withdraw of {arg1:float}', (arg1, callback) => {
+    myAccount.withdraw(arg1);
+    callback();
   });
-  When ('I make a withdraw of {arg1:float}', function (arg1, callback) {
-    myAccount.withdraw (arg1);
-    callback ();
-  });
-  Then ('the account\'s balance should be {arg1:float}', function (arg1, callback) {
-    assert.equal (myAccount.balance, arg1, 'The balance should be exactly ' + arg1);
-    callback ();
+
+  Then('the account\'s balance should be {arg1:float}', (arg1, callback) => {
+    assert.equal(myAccount.balance, arg1, `The balance should be exactly ${arg1}`);
+    callback();
   });
 });
